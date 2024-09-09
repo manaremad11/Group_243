3ff39a9a393072bd37048154caf70715f9c5eef4
manaremad11 <85031404+manaremad11@users.noreply.github.com>
2024-09-09 22:03:29 +0300

    Create B - qwerty.cpp

---
+++ Sheet(Loops)/B - qwerty.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    for(int i =1;i<=26;i++)
+    {
+        int n; 2
+        cin>>n;
+        char x='a'+(n-1);  
+        cout<<x;
+        
+    }    
+  
+    return 0;
+}
