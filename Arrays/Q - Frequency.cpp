e753029cc64e99b70029e3e55e05433a6a8f33e7
manaremad11 <85031404+manaremad11@users.noreply.github.com>
2024-09-16 23:44:56 +0300

    Create Q - Frequency.cpp

---
+++ Arrays/Q - Frequency.cpp
@@ -0,0 +1,23 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;                //5
+    int a[n];             //0--->4
+    int freq[n+1] = {0};    //5
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];   // 1     3 2 1 4
+
+       freq[a[i]]++; 
+        
+    }
+    for(int i=1;i<=n;i++)
+    {
+        cout<<i<<" : "<<freq[i]<<endl;
+    }
+    
+    
+}
