ed82be67c7dfa26c8a93bb02157d0cfe6e7b051e
manaremad11 <85031404+manaremad11@users.noreply.github.com>
2024-09-17 22:52:41 +0300

    Create E - Divisibility.cpp

---
+++ Conditions_Sheet/E - Divisibility.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin>>n>>m;
+    if(n%m==0)
+    {
+        cout<<"Yes"<<endl;
+    }
+    else
+    {
+        cout<<"No"<<endl;
+    }
+    
+    return 0;
+}
