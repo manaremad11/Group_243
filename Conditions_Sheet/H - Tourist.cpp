a90e13cf292fa9f879ac990658e56b6f7fbf2916
manaremad11 <85031404+manaremad11@users.noreply.github.com>
2024-09-17 23:17:16 +0300

    Create H - Tourist.cpp

---
+++ Conditions_Sheet/H - Tourist.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char c;
+    cin>>c;
+    if(c=='E'|| c=='e')
+    {
+        cout<<"Egyption"<<endl;
+    }
+    else
+    {
+        cout<<"Tourist"<<endl;
+    }
+     
+    
+    return 0;
+}
