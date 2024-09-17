d4e5c613b6e45d04b154468897373afd324248d5
manaremad11 <85031404+manaremad11@users.noreply.github.com>
2024-09-17 23:49:42 +0300

    Create L. The Brothers(assuitSheet).cpp

---
+++ Conditions_Sheet/L. The Brothers(assuitSheet).cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string n1,f1,n2,f2;
+    cin>>n1>>f1>>n2>>f2;
+    if(f1==f2)
+    {
+        cout<<"ARE BROTHERS"<<endl;
+    }
+    else
+    {
+        cout<<"NOT"<<endl;
+    }
+    
+  
+}
+    
+    
