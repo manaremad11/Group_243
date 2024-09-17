2d6a6c11130a5df2222c5d7cddd69194362dadb9
manaremad11 <85031404+manaremad11@users.noreply.github.com>
2024-09-17 22:28:16 +0300

    Create A - = 10.cpp

---
+++ Conditions_Sheet/A - = 10.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    if(n==10) 
+    {
+        cout<<"YES"<<endl;
+    }
+    
+    else
+    {
+        cout<<"NO"<<endl;
+    }
+    return 0;
+}
