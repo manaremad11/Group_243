57bc2b875588ff76d86b8b7615e2397be8100d01
manaremad11 <85031404+manaremad11@users.noreply.github.com>
2024-09-17 23:20:08 +0300

    Create I - Hot.cpp

---
+++ Conditions_Sheet/I - Hot.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    if(t>=30)
+    {
+        cout<<"Yes"<<endl;
+    }
+    else
+    {
+        cout<<"No"<<endl;
+    }
+    
+    return 0;
+}
